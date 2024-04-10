2356d5ea58a5efa3681241d45e8f5d835d41f3de
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-04-10 20:14:19 +0800

    Add endpoint "/groups/:id/expenses/:expense_id"

--- lib/splitmore_web/router.ex
+++ lib/splitmore_web/router.ex
@@ -35,6 +35,7 @@ defmodule SplitmoreWeb.Router do
       live "/groups/:id", GroupLive.Show, :show
       live "/groups/:id/show/edit", GroupLive.Show, :edit
       live "/groups/:id/expenses/new", ExpenseLive.Index, :new
+      live "/groups/:id/expenses/:expense_id", ExpenseLive.Show, :show
       live "/groups/:id/expenses/:expense_id/edit", ExpenseLive.Show, :edit
     end
   end
