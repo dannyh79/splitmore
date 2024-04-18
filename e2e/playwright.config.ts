45283d7f0450f30245824715227b579c42770f95
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-04-18 22:13:33 +0800

    Use env HEADLESS to toggle e2e headless option

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
+    headless: process.env.HEADLESS === 'false' ? false : true
   },
 
   /* Configure projects for major browsers */
