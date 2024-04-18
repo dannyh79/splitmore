da90c1c1692a89fa9b17b1555f13cc29177803bb
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-04-18 18:26:34 +0800

    Re-structure groups_test.exs

--- test/splitmore/groups_test.exs
+++ test/splitmore/groups_test.exs
@@ -2,36 +2,41 @@ defmodule Splitmore.GroupsTest do
   use Splitmore.DataCase
 
   alias Splitmore.Groups
+  alias Splitmore.Groups.Group
 
-  describe "groups" do
-    alias Splitmore.Groups.Group
+  import Splitmore.GroupsFixtures
 
-    import Splitmore.GroupsFixtures
+  @invalid_attrs %{name: nil}
 
-    @invalid_attrs %{name: nil}
-
-    test "list_groups/0 returns all groups" do
+  describe "list_groups/0" do
+    test "returns all groups" do
       group = group_fixture()
       assert Groups.list_groups() == [group]
     end
+  end
 
-    test "get_group!/1 returns the group with given id" do
+  describe "get_group!/1" do
+    test "returns the group with given id" do
       group = group_fixture()
       assert Groups.get_group!(group.id) == group
     end
+  end
 
-    test "create_group/1 with valid data creates a group" do
+  describe "create_group/1" do
+    test "creates a group" do
       valid_attrs = %{name: "some name"}
 
       assert {:ok, %Group{} = group} = Groups.create_group(valid_attrs)
       assert group.name == "some name"
     end
 
-    test "create_group/1 with invalid data returns error changeset" do
+    test "returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Groups.create_group(@invalid_attrs)
     end
+  end
 
-    test "update_group/2 with valid data updates the group" do
+  describe "update_group/2" do
+    test "updates the group" do
       group = group_fixture()
       update_attrs = %{name: "some updated name"}
 
@@ -39,19 +44,23 @@ defmodule Splitmore.GroupsTest do
       assert group.name == "some updated name"
     end
 
-    test "update_group/2 with invalid data returns error changeset" do
+    test "returns error changeset" do
       group = group_fixture()
       assert {:error, %Ecto.Changeset{}} = Groups.update_group(group, @invalid_attrs)
       assert group == Groups.get_group!(group.id)
     end
+  end
 
-    test "delete_group/1 deletes the group" do
+  describe "delete_group/1" do
+    test "deletes the group" do
       group = group_fixture()
       assert {:ok, %Group{}} = Groups.delete_group(group)
       assert_raise Ecto.NoResultsError, fn -> Groups.get_group!(group.id) end
     end
+  end
 
-    test "change_group/1 returns a group changeset" do
+  describe "change_group/1" do
+    test "returns a group changeset" do
       group = group_fixture()
       assert %Ecto.Changeset{} = Groups.change_group(group)
     end
