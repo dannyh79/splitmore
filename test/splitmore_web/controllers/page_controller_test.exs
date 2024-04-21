6129eac1863b0821236474a96649572778814346
PastLeo <chgu82837@gmail.com>
2024-04-22 01:14:33 +0800

    Add landing page and navbar (#2)
    * Add simple landing page and navbar
    
    * Forgot to add 2nd line of slogan

--- test/splitmore_web/controllers/page_controller_test.exs
+++ test/splitmore_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SplitmoreWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Splitmore: Splitwise in Elixir"
   end
 end
